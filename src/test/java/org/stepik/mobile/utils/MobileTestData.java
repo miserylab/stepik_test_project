bf9849dc38a237d21d3b8f1a59ed1d4fab72c527
Olga Demina <misertylab.r6s@gmail.com>
2023-09-10 21:17:48 +0300

    added MobileTestData.java

---
+++ src/test/java/org/stepik/mobile/utils/MobileTestData.java
@@ -0,0 +1,18 @@
+package org.stepik.mobile.utils;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class MobileTestData {
+
+    public String placeholderMessage = "Sign in to continue",
+            userEmail = "psychosomatic2032@gmail.com",
+            unregisteredEmail = "test@eryuew.com",
+            userPassword = "F8EhaLCsAp9sB5",
+            courseName = "Data Structures",
+            userName = "Olga D",
+            loginErrorMessageText = "Login/password was incorrect";
+
+    public List<String> categories = new ArrayList<>(Arrays.asList("Editors' choice", "Stepik trends", "My courses", "Popular"));
+}
